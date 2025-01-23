1f4343f834dfa2c9e220ecb60aae4c52b994988c
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-01-23 22:02:38 +0300

    Add fade animation to splash screen and navigate to HomeScreen after delay

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -8,12 +8,27 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
-  bool startAnimation = false;
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _fadeAnimation;
 
   @override
   void initState() {
     super.initState();
+
+    // Initialize the animation controller
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500), // Animation duration
+    );
+
+    // Initialize the fade animation
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(_animationController);
+
+    // Start the animation
+    _animationController.forward();
+
+    // Navigate to the HomeScreen after 5 seconds
     Future.delayed(const Duration(milliseconds: 5000), () {
       Navigator.pushReplacement(
         context,
@@ -23,17 +38,26 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   @override
+  void dispose() {
+    // Dispose the animation controller to avoid memory leaks
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white60,
       body: Center(
-        child: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage(
-                  'assets/bg/bg.png',
-                ),
-                scale: 2),
+        child: FadeTransition(
+          opacity: _fadeAnimation,
+          child: Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/bg/bg.png'),
+                scale: 2,
+              ),
+            ),
           ),
         ),
       ),
